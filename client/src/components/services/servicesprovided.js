3e65fec404f59913c514c13dbef22d5c3e942e7c
tmprakash3 <tammineni.maniprakash@gmail.com>
2020-04-24 15:37:51 +0530

    made changes for the checkbox

--- client/src/components/services/servicesprovided.js
+++ client/src/components/services/servicesprovided.js
@@ -8,7 +8,7 @@ import Checkbox from '@material-ui/core/Checkbox';
 export default function ServicesProvided(props) {
     
     const [checked, setChecked] = React.useState(true);
-    
+
     return (
         <React.Fragment>
             <Typography variant="h5" gutterBottom>
@@ -21,28 +21,28 @@ export default function ServicesProvided(props) {
                 <Grid container spacing={3}>
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" value={checked} name="collectFoodMenu" onChange={props.handleUserInfo} />}
+                            control={<Checkbox color="primary" value={checked} name="collectFoodMenu" onChange={props.handleUserInfo} />}
                             label="Collect Food Menu"
                         />
                     </Grid>
 
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" value={checked} name="perpareFood" onChange={props.handleUserInfo} />}
+                            control={<Checkbox color="primary" value={checked} name="perpareFood" onChange={props.handleUserInfo} />}
                             label="Prepare Food"
                         />
                     </Grid>
 
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" value={checked} name="deliveryFood" onChange={props.handleUserInfo} />}
+                            control={<Checkbox color="primary" value={checked} name="deliveryFood" onChange={props.handleUserInfo} />}
                             label="Delivering the Food"
                         />
                     </Grid>
 
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" value={checked} name="supplySuppliers"  onChange={props.handleUserInfo}  />}
+                            control={<Checkbox color="primary" value={checked} name="supplySuppliers"  onChange={props.handleUserInfo}  />}
                             label="Suppyling the suppliers"
                         />
                     </Grid>
@@ -55,56 +55,56 @@ export default function ServicesProvided(props) {
                 <Grid container spacing={3}>
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" value={checked} name="flowerDecoration"  onChange={props.handleUserInfo} />}
+                            control={<Checkbox color="primary" value={checked} name="flowerDecoration"  onChange={props.handleUserInfo} />}
                             label="Flower Decoration"
                         />
                     </Grid>
 
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" value={checked} name="stageDecoration" onChange={props.handleUserInfo} />}
+                            control={<Checkbox color="primary" value={checked} name="stageDecoration" onChange={props.handleUserInfo} />}
                             label="Stage Decoration"
                         />
                     </Grid>
 
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" value={checked} name="bridalMakeup" onChange={props.handleUserInfo} />}
+                            control={<Checkbox color="primary" value={checked} name="bridalMakeup" onChange={props.handleUserInfo} />}
                             label="Bridal Makeup"
                         />
                     </Grid>
 
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" value={checked} name="brideMakeup" onChange={props.handleUserInfo} />}
+                            control={<Checkbox color="primary" value={checked} name="brideMakeup" onChange={props.handleUserInfo} />}
                             label="Bride Groom Makeup"
                         />
                     </Grid>
 
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" name="haldiFunction" value={checked} onChange={props.handleUserInfo} />}
+                            control={<Checkbox color="primary" name="haldiFunction" value={checked} onChange={props.handleUserInfo} />}
                             label="Haldi Function"
                         />
                     </Grid>
 
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" name="mehendi" value={checked} onChange={props.handleUserInfo} />}
+                            control={<Checkbox color="primary" name="mehendi" value={checked} onChange={props.handleUserInfo} />}
                             label="Mehendi"
                         />
                     </Grid>
 
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" name="sangeeth" value={checked} onChange={props.handleUserInfo} />}
+                            control={<Checkbox color="primary" name="sangeeth" value={checked} onChange={props.handleUserInfo} />}
                             label="Sangeeth"
                         />
                     </Grid>
 
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" name="babyFunction" value={checked} onChange={props.handleUserInfo} />}
+                            control={<Checkbox color="primary" name="babyFunction" value={checked} onChange={props.handleUserInfo} />}
                             label="Baby First Bday Celebrations"
                         />
                     </Grid>
@@ -118,28 +118,28 @@ export default function ServicesProvided(props) {
                 <Grid container spacing={3}>
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" name="preWeddingShoot" value={checked} onChange={props.handleUserInfo} />}
+                            control={<Checkbox color="primary" name="preWeddingShoot" value={checked} onChange={props.handleUserInfo} />}
                             label="Pre Wedding Shoot"
                         />
                     </Grid>
 
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" name="postWeddingShoot" value={checked} onChange={props.handleUserInfo} />}
+                            control={<Checkbox color="primary" name="postWeddingShoot" value={checked} onChange={props.handleUserInfo} />}
                             label="Post Wedding Shoot"
                         />
                     </Grid>
 
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" name="albumDesign" value={checked} onChange={props.handleUserInfo} />}
+                            control={<Checkbox color="primary" name="albumDesign" value={checked} onChange={props.handleUserInfo} />}
                             label="Album Designing"
                         />
                     </Grid>
 
                     <Grid item xs={12} sm={3}>
                         <FormControlLabel
-                            control={<Checkbox color="secondary" name="saveTheDate" value={checked} onChange={props.handleUserInfo} />}
+                            control={<Checkbox color="primary" name="saveTheDate" value={checked} onChange={props.handleUserInfo} />}
                             label="Save the Date design"
                         />
                     </Grid>
